7f6df4c981bd890170cdb313bc616ad5b3e0ecef
josuealfar38475 <josuealfaro8441@gmail.com>
2026-09-10 18:04:26 -0700

    Replace EmailJS config with safe example

--- beach_circle_flutter/lib/email_config.dart
+++
@@ -1,5 +0,0 @@
-class EmailConfig {
-  static const String serviceId = 'service_sd8sdnd';
-  static const String templateId = 'template_zfb8lhs';
-  static const String publicKey = 'eNYMeVCN0f9NjXMcc';
-}
---
+++ beach_circle_flutter/lib/email_config.example.dart
@@ -0,0 +1,5 @@
+class EmailConfig {
+  static const String serviceId = 'YOUR_EMAILJS_SERVICE_ID';
+  static const String templateId = 'YOUR_EMAILJS_TEMPLATE_ID';
+  static const String publicKey = 'YOUR_EMAILJS_PUBLIC_KEY';
+}
